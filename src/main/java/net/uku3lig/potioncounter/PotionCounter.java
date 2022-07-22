9a78c99fec160d9a111c51f22ab4acdd276ee656
uku <uku3lig@gmail.com>
2022-07-22 14:09:35 +0200

    initial commit

---
+++ src/main/java/net/uku3lig/potioncounter/PotionCounter.java
@@ -0,0 +1,10 @@
+package net.uku3lig.potioncounter;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PotionCounter implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
