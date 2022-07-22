5299e649e6a100c5409015b5c613bec5242c2a19
uku <uku3lig@gmail.com>
2022-07-22 19:15:42 +0200

    show all types of potions in the hud

--- src/main/java/net/uku3lig/potioncounter/mixin/MixinInGameHud.java
+++ src/main/java/net/uku3lig/potioncounter/mixin/MixinInGameHud.java
@@ -7,8 +7,8 @@ import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionUtil;
-import net.minecraft.potion.Potions;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -17,10 +17,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.awt.*;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Mixin(InGameHud.class)
 public class MixinInGameHud {
-    private static final ItemStack HEAL_POT = PotionUtil.setPotion(new ItemStack(Items.SPLASH_POTION), Potions.HEALING);
+    private static final ItemStack SPLASH_POT = new ItemStack(Items.SPLASH_POTION);
     @Shadow @Final private MinecraftClient client;
 
     @Inject(method = "renderStatusEffectOverlay", at = @At("RETURN"))
@@ -30,13 +33,16 @@ public class MixinInGameHud {
         TextRenderer textRenderer = client.textRenderer;
         ItemRenderer itemRenderer = client.getItemRenderer();
 
-        long count = client.player.getInventory().main.stream()
-                .filter(i -> i.isItemEqual(HEAL_POT))
-                .count();
+        Map<Potion, Long> counts = client.player.getInventory().main.stream()
+                .filter(i -> i.isItemEqual(SPLASH_POT))
+                .collect(Collectors.groupingBy(PotionUtil::getPotion, Collectors.counting()));
 
-        ItemStack item = PotionUtil.setPotion(new ItemStack(Items.SPLASH_POTION, (int) count), Potions.HEALING);
+        List<ItemStack> items = counts.keySet().stream().map(p -> PotionUtil.setPotion(new ItemStack(Items.SPLASH_POTION, counts.get(p).intValue()), p)).toList();
 
-        itemRenderer.renderGuiItemIcon(item, 5, 5);
-        textRenderer.draw(matrices, String.valueOf(item.getCount()), 5 + 16 + 2f, 5 + textRenderer.fontHeight / 2f, Color.WHITE.getRGB());
+        for (int i = 0; i < items.size(); i++) {
+            ItemStack item = items.get(i);
+            itemRenderer.renderGuiItemIcon(item, 5, 5 + 16*i);
+            textRenderer.draw(matrices, String.valueOf(item.getCount()), 5 + 16 + 2f, 5 + 16*i + textRenderer.fontHeight / 2f, Color.WHITE.getRGB());
+        }
     }
 }
