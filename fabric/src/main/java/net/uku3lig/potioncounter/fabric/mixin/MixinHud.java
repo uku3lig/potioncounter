f119cca148814fe150a1688f2af7d670ec3bab43
uku <hi@uku.moe>
2026-06-15 19:03:56 +0200

    feat: 26.2-rc-2

--- fabric/src/main/java/net/uku3lig/potioncounter/fabric/mixin/MixinHud.java
+++ fabric/src/main/java/net/uku3lig/potioncounter/fabric/mixin/MixinHud.java
@@ -1,16 +1,16 @@
 package net.uku3lig.potioncounter.fabric.mixin;
 
 import net.minecraft.client.DeltaTracker;
-import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.Hud;
 import net.uku3lig.potioncounter.PotionCounter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = Gui.class, priority = 999)
-public class MixinGui {
+@Mixin(value = Hud.class, priority = 999)
+public class MixinHud {
     @Inject(method = "extractEffects", at = @At("HEAD"))
     private void afterRenderOverlay(GuiGraphicsExtractor graphics, DeltaTracker deltaTracker, CallbackInfo ci) {
         PotionCounter.extractCurrentPlayerPotions(graphics);
