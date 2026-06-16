6d329ae1bd65600c40f3164d7a7849403bc0886e
uku <hi@uku.moe>
2026-06-16 19:02:50 +0200

    chore: bump version to 1.12.0

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,7 +4,7 @@ object BuildConfig {
     const val NEOFORGE_VERSION: String = "26.2.0.0-beta"
     const val UKULIB_VERSION: String = "2.1.0+26.2"
 
-    const val MOD_VERSION: String = "1.11.0"
+    const val MOD_VERSION: String = "1.12.0"
 
     const val MODRINTH_PROJECT_ID: String = "JzdjByS4"
 
